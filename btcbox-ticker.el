06c9570833b78efb7264ae8baf9b60f6a18fab74
Zhe Lei <lzhes43@gmail.com>
2016-01-28 17:16:46 +0900

    Initial commit

---
+++ btcbox-ticker.el
@@ -0,0 +1,50 @@
+;;; btcbox-ticker --- show japanese bitcoin price on btcbox
+
+;; Copyright (C) 2016  Zhe Lei.
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+;;; Code:
+
+(defvar btcbox-ticker-mode-line " ¥0.00")
+
+(defvar btcbox-ticker-timer nil)
+
+
+(defun btcbox-ticker-poll-info ()
+  (let (json info sell-price buy-price)
+    (with-current-buffer (url-retrieve-synchronously "https://www.btcbox.co.jp/api/v1/ticker/")
+      (goto-char (point-min))
+      (re-search-forward "gzip" nil 'move)
+      (setq json (buffer-substring-no-properties (point) (point-max))))
+    (setq info (json-read-from-string json))
+    (setq btcbox-ticker-mode-line
+	  (format " [Sell:¥%s Buy:%s]"
+		  (assoc-default 'buy info)
+		  (assoc-default 'sell info)))
+    (list (assoc-default 'buy info) (assoc-default 'sell info))))
+
+
+(define-minor-mode btcbox-ticker-mode
+  "Minor mode to display the last btcbox price"
+  :init-value nil
+  :global t
+  :lighter btcbox-ticker-mode-line
+  (if btcbox-ticker-mode
+      (setq btcbox-ticker-timer
+	    (run-at-time "0 sec" 10 #'btcbox-ticker-poll-info))
+    (cancel-timer btcbox-ticker-timer)
+    (setq btcbox-ticker-timer nil)))
+
